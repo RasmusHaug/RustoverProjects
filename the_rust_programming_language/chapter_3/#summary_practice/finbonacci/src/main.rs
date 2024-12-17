233bfd7380937392a853d213d3158468025ad87c
RasmusHaug <97478497+RasmusHaug@users.noreply.github.com>
2024-12-17 06:39:56 +0000

    Update Fibonacci sequence code

--- the_rust_programming_language/chapter_3/#summary_practice/finbonacci/src/main.rs
+++ the_rust_programming_language/chapter_3/#summary_practice/finbonacci/src/main.rs
@@ -1,3 +1,29 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        println!("Type n'th POSITIVE fibonacci number to find:");
+        let mut option = String::new();
+
+        io::stdin()
+            .read_line(&mut option)
+            .expect("Failed to read line");
+
+        let value: u64 = match option.trim().parse() {
+            Ok(value) => value,
+            Err(_) => continue,
+        };
+
+        println!("fibonacci ({}) => {}", value, fibonacci(value))
+    }
+}
+
+fn fibonacci(n: u64) -> u64 {
+    if n == 0 {
+        0
+    } else if n == 1 {
+        1
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 }
